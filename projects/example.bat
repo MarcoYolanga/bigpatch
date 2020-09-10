f58c5c6ab8db80622434ad8067ea1af8f1ed00ed
MarcoYolanga <63655698+MarcoYolanga@users.noreply.github.com>
2020-09-10 21:47:06 +0200

    Update example.bat

--- projects/example.bat
+++ projects/example.bat
@@ -1,8 +1,12 @@
 rem USE FULL PATHS e.g: C:\Windows\Users\your_name\project
 
-set input_project_dir=[YOUR PROJECT'S DIR]
+
 rem the git's project folder
+set input_project_dir=[YOUR PROJECT'S DIR]
+
+rem where do you want the patch files to be copied in your local pc?
 set output_patch_dir=[YOUR OUTPUT FOLDER]
-rem where do you want the patch files to go?
+
+rem The name of your project, used to name the output folder (better with no spaces)
 set project_name=[YOUR PROJECT'S NAME]
-rem The name of your prooject
+
